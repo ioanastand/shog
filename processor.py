f3fbef20cbdc97b60c0ba0001a7ab0a641fe7434
ioanastand <ioanas@ukr.net>
2026-07-18 23:00:34 +0300

    Update processor.py

--- processor.py
+++ processor.py
@@ -1,1 +1,27 @@
-from PIL import Image
+from filters import Filters
+
+
+class Processor:
+
+    def process(
+        self,
+        images
+    ):
+
+        result = []
+
+        filters = Filters()
+
+        for image in images:
+
+            result.append({
+
+                "image": image,
+
+                "resize": filters.resize(image),
+
+                "convert": filters.convert(image)
+
+            })
+
+        return result
