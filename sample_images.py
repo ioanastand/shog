7a151ce4adbcf357071df4bc3b19279db9c1f54b
ioanastand <ioanas@ukr.net>
2026-07-18 23:04:15 +0300

    Create sample_images.py

---
+++ sample_images.py
@@ -0,0 +1,19 @@
+from image_file import ImageFile
+
+IMAGES = [
+
+    ImageFile(
+        "holiday.jpg",
+        1920,
+        1080,
+        "JPG"
+    ),
+
+    ImageFile(
+        "portrait.png",
+        1200,
+        1600,
+        "PNG"
+    )
+
+]
