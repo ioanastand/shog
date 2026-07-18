3ee061352e311b14edc26e9743db1c5e0a22fe00
ioanastand <ioanas@ukr.net>
2026-07-18 23:05:37 +0300

    Create exporter.py

---
+++ exporter.py
@@ -0,0 +1,18 @@
+class Exporter:
+
+    def export(
+        self,
+        processed
+    ):
+
+        print()
+
+        print("Export completed.")
+
+        print()
+
+        print(
+
+            f"Images processed: {len(processed)}"
+
+        )
