7b27d07909884e78b12fd72da500e91ac1b304d6
ioanastand <ioanas@ukr.net>
2026-07-18 23:02:16 +0300

    Add ImageFile class with initialization parameters

---
+++ image_file.py
@@ -0,0 +1,14 @@
+class ImageFile:
+
+    def __init__(
+        self,
+        name,
+        width,
+        height,
+        extension
+    ):
+
+        self.name = name
+        self.width = width
+        self.height = height
+        self.extension = extension
